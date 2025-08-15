cb90483003120d2472e738039fc3318cd1362ba3
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-08-15 14:33:44 +0300

    added order model

---
+++ models/order.js
@@ -0,0 +1,28 @@
+const {Schema, model} = require('mongoose')
+
+const orderSchema = new Schema ({
+    foodItem:{
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    notes: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    }
+})
+
+const Order = model ('Order', orderSchema)
+
+module.exports = Order
