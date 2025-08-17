ca67147f1a35b8c07414a5272441996b8d392366
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-08-17 10:42:08 +0300

    fixed menuRoutes names

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const menuController = require('../controllers/menu')
 
-router.get('/', menuController.allOrders)
-router.post('/', menuController.createOrder)
-router.get('/:id', menuController.showOrder)
-router.put('/:id', menuController.updateOrder)
-router.delete('/:id', menuController.deleteOrder)
+router.get('/', menuController.allMenus)
+router.post('/', menuController.createdMenu)
+router.get('/:id', menuController.showMenu)
+router.put('/:id', menuController.updateMenu)
+router.delete('/:id', menuController.deleteMenu)
 
 module.exports = router
